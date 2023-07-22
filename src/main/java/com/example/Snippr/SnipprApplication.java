6f777d315fdb45d34293c30828121899628bb0b6
Essa <essaessa1904@gmail.com>
2023-07-22 16:56:10 +0100

    first commit

---
+++ src/main/java/com/example/Snippr/SnipprApplication.java
@@ -0,0 +1,13 @@
+package com.example.Snippr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnipprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnipprApplication.class, args);
+	}
+
+}
